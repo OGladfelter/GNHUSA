32b00d597e9b464b1fd6fb8a9465a87cf35b74ee
OGladfelter <ogladfelter@gmail.com>
2022-10-19 12:59:07 -0500

    some lollipop styling

--- js/script.js
+++ js/script.js
@@ -264,107 +264,107 @@ function miniChart() {
     });
 }
 
-function packedCircles() {
-    // set the dimensions and margins of the graph
-    let box = document.getElementById('packedCircles');
-    let width = box.offsetWidth;
-    const height = width * 1.25;
+// function packedCircles() {
+//     // set the dimensions and margins of the graph
+//     let box = document.getElementById('packedCircles');
+//     let width = box.offsetWidth;
+//     const height = width * 1.25;
 
-    // append the svg object to the body of the page
-    const svg = d3.select("#packedCircles")
-    .append("svg")
-        .attr("width", width)
-        .attr("height", height);
+//     // append the svg object to the body of the page
+//     const svg = d3.select("#packedCircles")
+//     .append("svg")
+//         .attr("width", width)
+//         .attr("height", height);
 
-    const data = [
-        {
-            'name': 'Family',
-            'value': 45.06,
-            'group': 1
-        },
-        {
-            'name': 'Religion',
-            'value': 4.54,
-            'group': 2
-        },
-        {
-            'name': 'Finances',
-            'value': 6.18,
-            'group': 3
-        },
-        {
-            'name': 'Health',
-            'value': 6.32,
-            'group': 4
-        },
-    ];
+//     const data = [
+//         {
+//             'name': 'Family',
+//             'value': 45.06,
+//             'group': 1
+//         },
+//         {
+//             'name': 'Religion',
+//             'value': 4.54,
+//             'group': 2
+//         },
+//         {
+//             'name': 'Finances',
+//             'value': 6.18,
+//             'group': 3
+//         },
+//         {
+//             'name': 'Health',
+//             'value': 6.32,
+//             'group': 4
+//         },
+//     ];
     
-    // A scale that gives a Y target position for each group
-    const y = d3.scaleOrdinal()
-        .domain([1, 2, 3, 4])
-        .range([height * .1, height * .35, height * .6, height * .85])
+//     // A scale that gives a Y target position for each group
+//     const y = d3.scaleOrdinal()
+//         .domain([1, 2, 3, 4])
+//         .range([height * .1, height * .35, height * .6, height * .85])
 
-    // A color scale
-    const color = d3.scaleOrdinal()
-        .domain([1, 2, 3, 4])
-        .range([ "#26f0f1", "#e75a7c", "#5438dc", "#f0b67f"]);
+//     // A color scale
+//     const color = d3.scaleOrdinal()
+//         .domain([1, 2, 3, 4])
+//         .range([ "#26f0f1", "#e75a7c", "#5438dc", "#f0b67f"]);
 
-    // A size scale
-    const size = d3.scaleLinear()
-        .domain([0, 100])
-        .range([10, 100]);
+//     // A size scale
+//     const size = d3.scaleLinear()
+//         .domain([0, 100])
+//         .range([10, 100]);
 
-    const tooltip = d3.select("#tooltip");
+//     const tooltip = d3.select("#tooltip");
 
-    // Initialize the circle: all located at the center of the svg area
-    const node = svg.append("g")
-        .selectAll("circle")
-        .data(data)
-        .join("circle")
-            .attr("r", d => size(d.value))
-            .attr("cx", width / 2)
-            .attr("cy", height / 2)
-            .style("fill", d => color(d.group))
-            .attr('class', d => 'countryCircle' + d.group)
-            .style('opacity', 0)
-            .attr("stroke", "black")
-            .style("stroke-width", 1)
-            .attr("id", function(d) {
-                return d.name.toLowerCase() + 'Circle';
-            })
-            .on("mouseover", function(event, d) {
-                tooltip.html(d.name + "<br>" + d.value.toFixed(1) + "%")
-                .style('left', d.x + 'px')
-                    .style('top', d.y + 'px')
-                    .transition()
-                    .duration(250)
-                    .style('opacity', 1);
-            })
-            .on("mouseout", function() {
-                tooltip.transition().duration(250).style('opacity', 0);
-            });
+//     // Initialize the circle: all located at the center of the svg area
+//     const node = svg.append("g")
+//         .selectAll("circle")
+//         .data(data)
+//         .join("circle")
+//             .attr("r", d => size(d.value))
+//             .attr("cx", width / 2)
+//             .attr("cy", height / 2)
+//             .style("fill", d => color(d.group))
+//             .attr('class', d => 'countryCircle' + d.group)
+//             .style('opacity', 0)
+//             .attr("stroke", "black")
+//             .style("stroke-width", 1)
+//             .attr("id", function(d) {
+//                 return d.name.toLowerCase() + 'Circle';
+//             })
+//             .on("mouseover", function(event, d) {
+//                 tooltip.html(d.name + "<br>" + d.value.toFixed(1) + "%")
+//                 .style('left', d.x + 'px')
+//                     .style('top', d.y + 'px')
+//                     .transition()
+//                     .duration(250)
+//                     .style('opacity', 1);
+//             })
+//             .on("mouseout", function() {
+//                 tooltip.transition().duration(250).style('opacity', 0);
+//             });
 
-    // Features of the forces applied to the nodes:
-    var simulation = d3.forceSimulation()
-        .force("x", d3.forceX().strength(0.05).x(width / 2 ))
-        .force("y", d3.forceY().strength(0.05).y(height / 2 ))
-        .force("charge", d3.forceManyBody().strength(1)) // Nodes are attracted one each other of value is > 0
-        .force("collide", d3.forceCollide().strength(2).radius(50).iterations(20)) // Force that avoids circle overlapping
+//     // Features of the forces applied to the nodes:
+//     var simulation = d3.forceSimulation()
+//         .force("x", d3.forceX().strength(0.05).x(width / 2 ))
+//         .force("y", d3.forceY().strength(0.05).y(height / 2 ))
+//         .force("charge", d3.forceManyBody().strength(1)) // Nodes are attracted one each other of value is > 0
+//         .force("collide", d3.forceCollide().strength(2).radius(50).iterations(20)) // Force that avoids circle overlapping
 
-    // Apply these forces to the nodes and update their positions.
-    // Once the force algorithm is happy with positions ('alpha' value is low enough), simulations will stop.
-    simulation
-        .nodes(data)
-        .on("tick", function(d){
-        node
-            .attr("cx", d => d.x)
-            .attr("cy", d => d.y)
-        });
-}
+//     // Apply these forces to the nodes and update their positions.
+//     // Once the force algorithm is happy with positions ('alpha' value is low enough), simulations will stop.
+//     simulation
+//         .nodes(data)
+//         .on("tick", function(d){
+//         node
+//             .attr("cx", d => d.x)
+//             .attr("cy", d => d.y)
+//         });
+// }
 
 function lollipop() {
-    const margin = {top: 10, right: 30, bottom: 30, left: 70};
-    let box = document.getElementById('packedCircles');
+    const margin = {top: 10, right: 30, bottom: 30, left: 65};
+    let box = document.getElementById('lollipop');
     let width = box.offsetWidth - margin.left - margin.right;
     const height = (width * 1.25) - margin.top - margin.bottom;
 
@@ -376,20 +376,21 @@ function lollipop() {
         .append("g")
         .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
+    //d3.select("#lollipopTitle").style("transform", `translate(${margin.left}px, 0`);
+
     // Parse the Data
     d3.csv("data/themes.csv").then( function(data) {
 
-        // Add X axis
+        // add X axis
         const x = d3.scaleLinear()
             .domain([0, 50])
             .range([ 0, width]);
-            
         svg.append("g")
             .attr("class", "axis")
             .attr("transform", `translate(0, ${height})`)
             .call(d3.axisBottom(x).ticks(5).tickSizeOuter(0).tickFormat(function(d, i) { if (i == 0) return `${d}%`; else return d}));
 
-        // Y axis
+        // add Y axis
         const y = d3.scaleBand()
             .range([ 0, height ])
             .domain(data.map(function(d) { return d.theme; }))
@@ -397,6 +398,9 @@ function lollipop() {
         svg.append("g")
             .attr("class", "axis")
             .call(d3.axisLeft(y).tickSizeOuter(0).tickSize(0))
+            .selectAll("text")
+                .style("text-anchor", "start")
+                .style("transform", `translate(-${margin.left-2}px, 0`);
 
 
         // Lines
@@ -411,11 +415,11 @@ function lollipop() {
             .attr("stroke", "black")
             .attr('id', function(d) { return d.theme + "Line"; });
 
-        const color = d3.scaleOrdinal()
-            .domain(['Health', 'Family', 'Finances', 'Religion'])
-            .range([ "#f0b67f", "#26f0f1", "#5438dc", "#e75a7c"]);
+        // const color = d3.scaleOrdinal()
+        //     .domain(['Health', 'Family', 'Finances', 'Religion'])
+        //     .range([ "#f0b67f", "#26f0f1", "#5438dc", "#e75a7c"]);
 
-        // Circles
+        // Circles - overall
         svg.selectAll("mycircle")
             .data(data)
             .enter()
@@ -423,9 +427,21 @@ function lollipop() {
             .attr("cx", function(d) { return x(0); })  // start not showing line for animation later
             .attr("cy", function(d) { return y(d.theme); })
             .attr("r", 0)  // start not showing line for animation later
-            .style("fill", d => color(d.theme))
+            .style("fill", primaryColor)
             .attr("stroke", "black")
             .attr('id', function(d) { return d.theme + "Circle"; });
+
+        // Circles - young cohort
+        // svg.selectAll("mycircle")
+        //     .data(data)
+        //     .enter()
+        //     .append("circle")
+        //     .attr("cx", function(d) { return x(0); })  // start not showing line for animation later
+        //     .attr("cy", function(d) { return y(d.theme); })
+        //     .attr("r", 8)  // start not showing line for animation later
+        //     .style("fill", primaryColor)
+        //     .attr("stroke", "black")
+        //     .attr('id', function(d) { return d.theme + "CircleYoung"; });
 
         new Waypoint({
             element: document.getElementById('family'),
